667c280427f8ab56ee0e9ff7cb054f73f7e9ba9d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-15 23:40:00 +0000

    update the about page description

--- src/About/About.js
+++ src/About/About.js
@@ -8,8 +8,8 @@ function About() {
       <p>
         Nature Sounds have been linked to relaxation and less stress for hundred
         of years. The sounds in our library help you focus better and be more
-        productive. According to science nature sounds help you soothe your
-        stress by altering certain connections in our brain
+        productive. According to science, nature sounds help you soothe your
+        stress by altering certain connections in our brain.
       </p>
     </div>
   );
